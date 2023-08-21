4c225d748f63146dbfea87febf4cd090c5a4c457
bavidowie <axmetsh@gmail.com>
2023-08-22 00:24:45 +0300

    promisereduce

---
+++ promisereduce.js
@@ -0,0 +1,33 @@
+var fn1 = () => {
+  console.log("fn1");
+  return Promise.resolve(1);
+};
+var fn2 = () =>
+  new Promise((resolve) => {
+      console.log("fn2");
+      setTimeout(() => resolve(2), 1000);
+  });
+function promiseReduce(asyncFunctions, reduce, initialValue) {
+  return new Promise ((resolve) => {
+      let Accumulator = initialValue
+      asyncFunctions.reduce((prevPromise, currentFunc, index) =>
+          prevPromise.then(val => {
+              if (index !== 0) {
+                Accumulator = reduce(Accumulator, val)
+              }
+              return currentFunc()
+          })
+      , Promise.resolve(initialValue)).then(val => {
+        resolve(reduce(Accumulator, val))
+      })
+  })
+}
+promiseReduce(
+  [fn1, fn2],
+  function (memo, value) {
+      console.log("reduce");
+
+      return memo * value;
+  },
+  1
+).then(console.log);
